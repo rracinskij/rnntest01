ea57d7810a772c712867f45293a532d1cee6349a
rracinskij <r.racinskij@gmail.com>
2015-12-17 01:33:26 +0200

    Update rnntest01.lua
    Linear input layer

--- rnntest01.lua
+++ rnntest01.lua
@@ -1,14 +1,14 @@
 -- This is a modification of an example provided at https://github.com/Element-Research/rnn#rnn.Recurrent
+-- Please refer to https://github.com/tindzk/rnntest01 for a detailed example
 require 'rnn'
 
 --batchSize = 1
 rho = 5
 hiddenSize = 10
-nIndex = 20
 -- RNN
 r = nn.Recurrent(
    hiddenSize, --size of the input layer
-   nn.LookupTable(nIndex, hiddenSize), --input layer (inputSize, outputSize) (use nn.Linear to work with numbers <1)
+   nn.Linear(1, hiddenSize), --input layer
    nn.Linear(hiddenSize, hiddenSize), --recurrent layer
    nn.Sigmoid(), --transfer function
    rho  --maximum number of time steps for BPTT
@@ -22,9 +22,9 @@ criterion = nn.MSECriterion()
 
 -- dummy dataset (task is to predict next item, given previous)
 i = 0
-sequence = torch.Tensor(nIndex):apply(function() --fill with a simple arithmetic progression 1,2..9,1,2...
-  i = i + 1
-  if i == 10 then i = 1 end
+sequence = torch.Tensor(10):apply(function() --fill with a simple arithmetic progression 0.1, 0.2, .. 0.9, 1, 0.1,..
+  i = i + 0.1
+  if i >1 then i = 0 end
   return i
 end)
 print('Sequence:')
@@ -36,14 +36,14 @@ threshold = 0.002
 thresholdStep = 0
 --while true do
 for k = 1, 100 do
-for j = 1, 9 do
+for j = 1, sequence:size(1)-1 do
    step = step + 1
    -- a batch of inputs
    local input = torch.Tensor(1):fill(sequence[j])
    local output = rnn:forward(input)
    local target = torch.Tensor(1):fill(sequence[j+1]) --target is the next numbet in sequence
    local err = criterion:forward(output, target)
-   print('Step: ', step, ' Input: ', input[1], ' Target: ', target[1], ' Output: ', output[1][1], ' Error: ', err)
+   print('Step: ', step, ' Input: ', input[1], ' Target: ', target[1], ' Output: ', output[1], ' Error: ', err)
    if (err < threshold and thresholdStep == 0) then thresholdStep = step end --remember this step
    local gradOutput = criterion:backward(output, target)
    -- the Recurrent layer is memorizing its gradOutputs (up to memSize)
